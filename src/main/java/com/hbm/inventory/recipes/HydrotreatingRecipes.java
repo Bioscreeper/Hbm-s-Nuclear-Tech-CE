118891bb977be3b7ff399bc2ef58d6605016f6a6
Bioscreeper <67083372+Bioscreeper@users.noreply.github.com>
2026-04-09 23:35:54 +1000

    hydrotreater disease (toxic compound)
    Fix hydrotreater's outputs being 10x too small
    also hydrogen

--- src/main/java/com/hbm/inventory/recipes/HydrotreatingRecipes.java
+++ src/main/java/com/hbm/inventory/recipes/HydrotreatingRecipes.java
@@ -70,10 +70,10 @@ public class HydrotreatingRecipes extends SerializableRecipe {
         for(Map.Entry<FluidType, Tuple.Triplet<FluidStack, FluidStack, FluidStack>> recipe : recipes.entrySet()) {
             map.put(new ItemStack[] {
                             ItemFluidIcon.make(recipe.getKey(), 1000),
-                            ItemFluidIcon.make(recipe.getValue().getX().type,	recipe.getValue().getX().fill * 10, 1) },
+                            ItemFluidIcon.make(recipe.getValue().getX().type,	recipe.getValue().getX().fill * 100, 1) },
                     new ItemStack[] {
-                            ItemFluidIcon.make(recipe.getValue().getY().type,	recipe.getValue().getY().fill * 10),
-                            ItemFluidIcon.make(recipe.getValue().getZ().type,	recipe.getValue().getZ().fill * 10) });
+                            ItemFluidIcon.make(recipe.getValue().getY().type,	recipe.getValue().getY().fill * 100),
+                            ItemFluidIcon.make(recipe.getValue().getZ().type,	recipe.getValue().getZ().fill * 100) });
         }
 
         return map;
